b8fdfaed2ec432eab466c28fea1d9a0f61a59476
Troy <trmaloney94@gmail.com>
2018-10-14 13:59:31 -0700

    Modified deactivate_color method. Will not work with current create_string method.

--- led_gui.py
+++ led_gui.py
@@ -27,6 +27,10 @@ class ColorChanger:
         self.red_ON = True
         self.blue_ON = True
         self.green_ON = True
+        
+        self.val_dict = {'red':self.red_val, 'blue':self.blue_val, 'green':self.green_val}
+        self.old_dict = {'red':self.old_red, 'blue':self.old_blue, 'green':self.old_green}
+        self.on_dict = {'red':self.red_ON, 'blue':self.blue_ON, 'green':self.green_ON}
 
         try:
             self.ser = serial.Serial(serial_port, 9600)
@@ -80,6 +84,12 @@ class ColorChanger:
         if self.no_dweeno:
             no_dweeno_message.grid(row=3,column=0, columnspan=4)
 
+#-- PLACE WIDGETS IN DICTIONARIES --#
+        # sliders, val displays, toggles, 
+        self.slider_dict = {'red':self.slider_red, 'blue':self.slider_blue, 'green':self.slider_green}        
+        self.val_display_dict = {'red':self.red_val_display,'blue':self.blue_val_display,'green':self.green_val_display}
+        self.etoggle_dict = {'red':self.toggle_red,'blue':self.toggle_blue,'green':self.toggle_green}
+
 # --FUNCTINOS-- #
     def create_string(self):
         return str(self.red_val)+','+str(self.green_val)+','+str(self.blue_val)+'>'
@@ -118,95 +128,109 @@ class ColorChanger:
             self.ser.write(output)
 
     def deactivate_color(self, color):
-        if color == 'red':
-            if self.red_ON:
-                self.red_val = 0
-                self.old_red = self.slider_red.get()
-
-                self.red_val_display.config(text=self.scale_for_display(self.red_val))
-                self.slider_red.config(state=tk.DISABLED, sliderrelief=tk.SUNKEN, relief=tk.RIDGE)
-                self.toggle_red.config(relief=tk.SUNKEN, text='Off')
-
-                if self.no_dweeno:
-                    print self.create_string()
-                else:
-                    self.ser.write(self.create_string())
-
-                self.red_ON = False
-
+    
+        if self.on_dict[color]:
+            self.val_dict[color] = 0
+            self.old_dict[color] = self.slider_dict[color].get()
+            
+            self.val_display_dict[color].config(text=self.scale_for_display(self.val_dict[color] ) )
+            self.slider_dict[color].config(state=tk.DISABLED, sliderrelief=tk.SUNKEN, relief=tk.RIDGE)
+            self.toggle_dict[color].config(relief=tk.SUNKEN, text='Off')
+            
+            if self.no_dweeno:
+                print self.create_string()
             else:
-                self.red_val = self.old_red
-                
-                self.red_val_display.config(text=self.scale_for_display(self.red_val))
-                self.slider_red.config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
-                self.toggle_red.config(relief=tk.RAISED, text='On')
-                
-                if self.no_dweeno:
-                    print self.create_string()
-                else:
-                    self.ser.write(self.create_string())
-                
-                self.red_ON = True
-
-        elif color == 'blue':
-            if self.blue_ON:
-                self.blue_val = 0
-                self.old_blue = self.slider_blue.get()
-
-                self.blue_val_display.config(text=self.scale_for_display(self.blue_val))
-                self.slider_blue.config(state=tk.DISABLED, sliderrelief=tk.SUNKEN, relief=tk.RIDGE)
-                self.toggle_blue.config(relief=tk.SUNKEN, text='Off')
-
-                if self.no_dweeno:
-                    print self.create_string()
-                else:
-                    self.ser.write(self.create_string())
-
-                self.blue_ON = False
-
+                self.ser.write(self.create_string() )
+            
+            self.on_dict[color] = False
+            
+        else:
+            self.val_dict[color] = self.old_dict[color]
+            
+            self.val_display_dict[color].config(text=self.scale_for_display(self.val_dict[color]))
+            self.slider_dict[color].config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
+            self.toggle_dict[color].config(relief=tk.RAISED, text="On")
+            
+            if self.no_dweeno:
+                print self.create_string()
             else:
-                self.blue_val = self.old_blue
-                
-                self.blue_val_display.config(text=self.scale_for_display(self.blue_val))
-                self.slider_blue.config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
-                self.toggle_blue.config(relief=tk.RAISED, text='On')
-                
-                if self.no_dweeno:
-                    print self.create_string()
-                else:
-                    self.ser.write(self.create_string())
-                
-                self.blue_ON = True
-             
-        elif color == 'green':
-            if self.green_ON:
-                self.green_val = 0
-                self.old_green = self.slider_green.get()
+                self.ser.write(self.create_string() )
+            
+            self.on_dict[color] = True
 
-                self.green_val_display.config(text=self.scale_for_display(self.green_val))
-                self.slider_green.config(state=tk.DISABLED, sliderrelief=tk.SUNKEN, relief=tk.RIDGE)
-                self.toggle_green.config(relief=tk.SUNKEN, text='Off')
+#            else:
+#                self.red_val = self.old_red
+#                
+#                self.red_val_display.config(text=self.scale_for_display(self.red_val))
+#                self.slider_red.config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
+#                self.toggle_red.config(relief=tk.RAISED, text='On')
+#                
+#                if self.no_dweeno:
+#                    print self.create_string()
+#                else:
+#                    self.ser.write(self.create_string())
+#                
+#                self.red_ON = True
 
-                if self.no_dweeno:
-                    print self.create_string()
-                else:
-                    self.ser.write(self.create_string())
+#        elif color == 'blue':
+#            if self.blue_ON:
+#                self.blue_val = 0
+#                self.old_blue = self.slider_blue.get()
 
-                self.green_ON = False
+#                self.blue_val_display.config(text=self.scale_for_display(self.blue_val))
+#                self.slider_blue.config(state=tk.DISABLED, sliderrelief=tk.SUNKEN, relief=tk.RIDGE)
+#                self.toggle_blue.config(relief=tk.SUNKEN, text='Off')
 
-            else:
-                self.green_val = self.old_green
-                
-                self.green_val_display.config(text=self.scale_for_display(self.green_val))
-                self.slider_green.config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
-                self.toggle_green.config(relief=tk.RAISED, text='On')
-                
-                if self.no_dweeno:
-                    print self.create_string()
-                else:
-                    self.ser.write(self.create_string())
-                
-                self.green_ON = True
+#                if self.no_dweeno:
+#                    print self.create_string()
+#                else:
+#                    self.ser.write(self.create_string())
+
+#                self.blue_ON = False
+
+#            else:
+#                self.blue_val = self.old_blue
+#                
+#                self.blue_val_display.config(text=self.scale_for_display(self.blue_val))
+#                self.slider_blue.config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
+#                self.toggle_blue.config(relief=tk.RAISED, text='On')
+#                
+#                if self.no_dweeno:
+#                    print self.create_string()
+#                else:
+#                    self.ser.write(self.create_string())
+#                
+#                self.blue_ON = True
+#             
+#        elif color == 'green':
+#            if self.green_ON:
+#                self.green_val = 0
+#                self.old_green = self.slider_green.get()
+
+#                self.green_val_display.config(text=self.scale_for_display(self.green_val))
+#                self.slider_green.config(state=tk.DISABLED, sliderrelief=tk.SUNKEN, relief=tk.RIDGE)
+#                self.toggle_green.config(relief=tk.SUNKEN, text='Off')
+
+#                if self.no_dweeno:
+#                    print self.create_string()
+#                else:
+#                    self.ser.write(self.create_string())
+
+#                self.green_ON = False
+
+#            else:
+#                self.green_val = self.old_green
+#                
+#                self.green_val_display.config(text=self.scale_for_display(self.green_val))
+#                self.slider_green.config(state=tk.NORMAL, sliderrelief=tk.RAISED, relief=tk.FLAT)
+#                self.toggle_green.config(relief=tk.RAISED, text='On')
+#                
+#                if self.no_dweeno:
+#                    print self.create_string()
+#                else:
+#                    self.ser.write(self.create_string())
+#                
+#                self.green_ON = True
 
 
 
